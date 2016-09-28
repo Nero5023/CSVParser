d9acf6d1615959715022539711eaf45f5b5fa3d9
Nero Zuo <502350123@qq.com>
2016-09-28 17:44:05 +0800

    Add barriers to store(write) the parsed lines to rows property

--- Sources/CSVParser.swift
+++ Sources/CSVParser.swift
@@ -1,29 +1,64 @@
 import Foundation
 
-
 class CSVParser {
   
   var content: String
   
   let delimiter: Character
   var lines: [String]
-  
+  var rows: [[String]]
   
   init(content: String, delimiter: Character = ",") {
     self.content = content
     self.delimiter = delimiter
     self.lines = content.lines()
+    // 3?
+    self.rows = Array<[String]>.init(repeating: [String].init(repeating: "", count: 3), count: self.lines.count)
   }
 
   convenience init(filePath: String, delimiter: Character = ",") throws {
     let fileContent = try String(contentsOfFile: filePath)
     self.init(content: fileContent, delimiter: delimiter)
-    
   }
   
   func wirite(toFilePath path: String) throws {
     try self.lines.joined(separator: "\r\n").write(to: URL(fileURLWithPath: path), atomically: false, encoding: .utf8)
   }
+  
+  func concurrencyParse(handler:  @escaping ()->()) {
+    let wordsInOneTime = 200
+    let parseGroup = DispatchGroup()
+    let writeRowQueue = DispatchQueue(label: "com.nero.writerow")
+    writeRowQueue.setTarget(queue: DispatchQueue.global(qos: .default))
+    for i in 0...self.lines.count / wordsInOneTime {
+      let workItem = DispatchWorkItem(block: {
+        let min = wordsInOneTime < (self.lines.count - i*wordsInOneTime) ? wordsInOneTime : (self.lines.count - i*wordsInOneTime)
+        for j in 0..<min{
+          let index = i*wordsInOneTime + j
+//          self.rows[index] =
+          let parsedLine = self.lines[index].words()
+//          dispatch_barrier_async(<#T##queue: DispatchQueue##DispatchQueue#>, <#T##block: () -> Void##() -> Void#>)
+          writeRowQueue.async(group: parseGroup, qos: .default, flags: .barrier) {
+            self.rows[index] = parsedLine
+          }
+        }
+        print("\(i) done")
+      })
+      DispatchQueue.global(qos: .userInitiated).async(group: parseGroup, execute: workItem)
+
+    }
+//    parseGroup.notify(queue: DispatchQueue.main, execute: handler)
+    parseGroup.wait()
+    handler()
+  }
+  
+  func parse() {
+    for (index, line) in self.lines.enumerated() {
+      self.rows[index] = line.words()
+    }
+  }
+  
+  
 }
 
 extension String {
